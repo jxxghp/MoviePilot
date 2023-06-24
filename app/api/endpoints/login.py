eb37e66a5e8ae21a12ad8a71dc4e0444559b8406
jxxghp <jxxghp@gmail.com>
2023-06-24 16:21:16 +0800

    fix login error code

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -34,12 +34,12 @@ async def login_access_token(
         logger.warn("登录用户本地不匹配，尝试辅助认证 ...")
         token = UserChain().user_authenticate(form_data.username, form_data.password)
         if not token:
-            raise HTTPException(status_code=400, detail="用户名或密码不正确")
+            raise HTTPException(status_code=401, detail="用户名或密码不正确")
         else:
             logger.info(f"辅助认证成功，用户信息: {token}")
             user = schemas.User(id=-1, name=form_data.username, is_active=True, is_superuser=False)
     elif not user.is_active:
-        raise HTTPException(status_code=400, detail="用户未启用")
+        raise HTTPException(status_code=401, detail="用户未启用")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return schemas.Token(
         access_token=security.create_access_token(
