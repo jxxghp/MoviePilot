e732c4005e39409aa0d51e87a79e6b70ea9847e1
jxxghp <jxxghp@gmail.com>
2023-07-02 18:27:03 +0800

    fix

--- app/api/endpoints/download.py
+++ app/api/endpoints/download.py
@@ -18,15 +18,26 @@ async def read_downloading(
     return DownloadChain().downloading()
 
 
-@router.put("/{hashString}", summary="开始/暂停", response_model=schemas.Response)
-async def set_downloading(
+@router.put("/{hashString}/start", summary="开始任务", response_model=schemas.Response)
+async def start_downloading(
         hashString: str,
-        oper: str,
+        _: schemas.TokenPayload = Depends(verify_token)) -> Any:
+    """
+    开如下载任务
+    """
+    ret = DownloadChain().set_downloading(hashString, "start")
+    return schemas.Response(success=True if ret else False)
+
+
+@router.put("/{hashString}/stop", summary="暂停任务", response_model=schemas.Response)
+async def stop_downloading(
+        hashString: str,
         _: schemas.TokenPayload = Depends(verify_token)) -> Any:
     """
     控制下载任务
     """
-    return DownloadChain().set_downloading(hashString, oper)
+    ret = DownloadChain().set_downloading(hashString, "stop")
+    return schemas.Response(success=True if ret else False)
 
 
 @router.delete("/{hashString}", summary="删除下载任务", response_model=schemas.Response)
@@ -36,4 +47,5 @@ async def remove_downloading(
     """
     控制下载任务
     """
-    return DownloadChain().remove_downloading(hashString)
+    ret = DownloadChain().remove_downloading(hashString)
+    return schemas.Response(success=True if ret else False)
