0573d6beff12419f5b73d46ea9330e681ea276ce
jxxghp <jxxghp@gmail.com>
2023-07-04 22:19:59 +0800

    fix

--- app/helper/message.py
+++ app/helper/message.py
@@ -1,7 +1,9 @@
 import queue
 
+from app.utils.singleton import Singleton
 
-class MessageHelper:
+
+class MessageHelper(metaclass=Singleton):
     """
     消息队列管理器
     """
@@ -12,4 +14,4 @@ class MessageHelper:
         self.queue.put(message)
 
     def get(self):
-        return self.queue.get(block=True)
+        return self.queue.get(block=False)
