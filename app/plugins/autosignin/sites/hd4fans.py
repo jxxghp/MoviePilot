142e57c5608281f45d5e6fb99bfe805671725d47
thsrite <thsrite@11>
2023-07-11 10:09:21 +0800

    fix

--- app/plugins/autosignin/sites/hd4fans.py
+++ app/plugins/autosignin/sites/hd4fans.py
@@ -1,9 +1,15 @@
-from app.plugins.modules._autosignin._base import _ISiteSigninHandler
-from app.utils import StringUtils, RequestUtils
-from config import Config
+from typing import Tuple
+
+from ruamel.yaml import CommentedMap
+
+from app.core.config import settings
+from app.log import logger
+from app.plugins.autosignin.sites import _ISiteSigninHandler
+from app.utils.http import RequestUtils
+from app.utils.string import StringUtils
 
 
-class HD4FANS(_ISiteSigninHandler):
+class HDArea(_ISiteSigninHandler):
     """
     兽签到
     """
@@ -16,7 +22,7 @@ class HD4FANS(_ISiteSigninHandler):
     _success_text = "签到成功"
 
     @classmethod
-    def match(cls, url):
+    def match(cls, url: str) -> bool:
         """
         根据站点Url判断是否匹配当前站点签到类，大部分情况使用默认实现即可
         :param url: 站点Url
@@ -24,7 +30,7 @@ class HD4FANS(_ISiteSigninHandler):
         """
         return True if StringUtils.url_equal(url, cls.site_url) else False
 
-    def signin(self, site_info: dict):
+    def signin(self, site_info: CommentedMap) -> Tuple[bool, str]:
         """
         执行签到操作
         :param site_info: 站点信息，含有站点Url、站点Cookie、UA等信息
@@ -33,24 +39,24 @@ class HD4FANS(_ISiteSigninHandler):
         site = site_info.get("name")
         site_cookie = site_info.get("cookie")
         ua = site_info.get("ua")
-        proxy = Config().get_proxies() if site_info.get("proxy") else None
+        proxies = settings.PROXY if site_info.get("proxy") else None
 
         # 获取页面html
         html_res = RequestUtils(cookies=site_cookie,
                                 headers=ua,
-                                proxies=proxy
+                                proxies=proxies
                                 ).get_res(url="https://pt.hd4fans.org/index.php")
         if not html_res or html_res.status_code != 200:
-            self.error(f"签到失败，请检查站点连通性")
+            logger.error(f"签到失败，请检查站点连通性")
             return False, f'【{site}】签到失败，请检查站点连通性'
 
         if "login.php" in html_res.text:
-            self.error(f"签到失败，cookie失效")
+            logger.error(f"签到失败，cookie失效")
             return False, f'【{site}】签到失败，cookie失效'
 
         # 判断是否已签到
         if self._repeat_text in html_res.text:
-            self.info(f"今日已签到")
+            logger.info(f"今日已签到")
             return True, f'【{site}】今日已签到'
 
         # 签到
@@ -59,14 +65,14 @@ class HD4FANS(_ISiteSigninHandler):
         }
         sign_res = RequestUtils(cookies=site_cookie,
                                 headers=ua,
-                                proxies=proxy
+                                proxies=proxies
                                 ).post_res(url="https://pt.hd4fans.org/checkin.php", data=data)
         if not sign_res or sign_res.status_code != 200:
-            self.error(f"签到失败，请检查站点连通性")
+            logger.error(f"签到失败，请检查站点连通性")
             return False, f'【{site}】签到失败，请检查站点连通性'
         if not sign_res.text and int(sign_res.text) > 0:
-            self.info(f"签到成功")
+            logger.info(f"签到成功")
             return True, f'【{site}】签到成功'
 
-        self.error(f"签到失败，签到接口返回 {sign_res.text}")
+        logger.error(f"签到失败，签到接口返回 {sign_res.text}")
         return False, f'【{site}】签到失败'
