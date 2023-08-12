99aa07348fa009f6cced6e2ecc1783270761e02b
thsrite <thsrite@gmail.com>
2023-08-12 14:32:47 +0800

    fix

--- app/api/endpoints/webhook.py
+++ app/api/endpoints/webhook.py
@@ -16,22 +16,7 @@ def start_webhook_chain(body: Any, form: Any, args: Any):
     WebhookChain().message(body=body, form=form, args=args)
 
 
-@router.post("/", summary="Webhook消息响应", response_model=schemas.Response)
-async def webhook_message(background_tasks: BackgroundTasks,
-                          token: str, request: Request) -> Any:
-    """
-    Webhook响应
-    """
-    if token != settings.API_TOKEN:
-        return schemas.Response(success=False, message="token认证不通过")
-    body = await request.body()
-    form = await request.form()
-    args = request.query_params
-    background_tasks.add_task(start_webhook_chain, body, form, args)
-    return schemas.Response(success=True)
-
-
-@router.get("/", summary="Webhook消息响应", response_model=schemas.Response)
+@router.route(path="/", methods=['GET', 'POST'], name="Webhook消息响应")
 async def webhook_message(background_tasks: BackgroundTasks,
                           token: str, request: Request) -> Any:
     """
