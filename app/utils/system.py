27b1e0ffd5fe75c46cc493eac05a938147e1e05e
jxxghp <jxxghp@gmail.com>
2023-09-27 09:47:56 +0800

    fix #668 rollback #654

--- app/utils/system.py
+++ app/utils/system.py
@@ -349,9 +349,9 @@ class SystemUtils:
             # 获取当前容器的 ID
             with open('/proc/self/mountinfo', 'r') as f:
                 data = f.read()
-                index_resolv_conf = data.find("/sys/fs/cgroup/devices")
+                index_resolv_conf = data.find("resolv.conf")
                 if index_resolv_conf != -1:
-                    index_second_slash = data.rfind(" ", 0, index_resolv_conf)
+                    index_second_slash = data.rfind("/", 0, index_resolv_conf)
                     index_first_slash = data.rfind("/", 0, index_second_slash) + 1
                     container_id = data[index_first_slash:index_second_slash]
             if not container_id:
