493b7c2d2483ed9608038f281ac4bdf6245f933f
thsrite <thsrite@gmail.com>
2023-10-14 20:49:03 +0800

    fix 重启系统发送消息

--- app/chain/system.py
+++ app/chain/system.py
@@ -2,12 +2,14 @@ from typing import Union
 
 from app.chain import ChainBase
 from app.schemas import Notification, MessageChannel
+from app.utils.system import SystemUtils
 
 
 class SystemChain(ChainBase):
     """
     系统级处理链
     """
+
     def remote_clear_cache(self, channel: MessageChannel, userid: Union[int, str]):
         """
         清理系统缓存
@@ -15,3 +17,11 @@ class SystemChain(ChainBase):
         self.clear_cache()
         self.post_message(Notification(channel=channel,
                                        title=f"缓存清理完成！", userid=userid))
+
+    def restart(self, channel: MessageChannel, userid: Union[int, str]):
+        """
+        重启系统
+        """
+        self.post_message(Notification(channel=channel,
+                                       title=f"系统正在重启，请耐心等候！", userid=userid))
+        SystemUtils.restart()
