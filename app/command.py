4875db08e8b3752e37179d868f64bc5f377dfcde
jxxghp <jxxghp@gmail.com>
2023-10-20 12:48:43 +0800

    fix #927

--- app/command.py
+++ app/command.py
@@ -103,7 +103,10 @@ class Command(metaclass=Singleton):
                 "id": "subscribe_search",
                 "type": "scheduler",
                 "description": "搜索订阅",
-                "category": "订阅"
+                "category": "订阅",
+                "kwargs": {
+                    'state': 'R'
+                }
             },
             "/subscribe_delete": {
                 "func": SubscribeChain().remote_delete,
@@ -230,7 +233,7 @@ class Command(metaclass=Singleton):
                 )
 
             # 执行定时任务
-            self.scheduler.start(job_id=command.get("id"))
+            self.scheduler.start(job_id=command.get("id"), **command.get("kwargs", {}))
 
             if userid:
                 self.chain.post_message(
