099dcda1855b7636299b80aa14423e0f11d0d28b
thsrite <thsrite@gmail.com>
2023-10-31 16:33:48 +0800

    fix webhook

--- app/modules/emby/emby.py
+++ app/modules/emby/emby.py
@@ -816,7 +816,9 @@ class Emby(metaclass=Singleton):
                         "E" + str(message.get('Item', {}).get('IndexNumber')),
                         message.get('Item', {}).get('Name'))
                 else:
-                    eventItem.item_name = message.get('Item', {}).get('Name')
+                    eventItem.item_name = "%s %s" % (
+                        message.get('Item', {}).get('SeriesName'),
+                        message.get('Item', {}).get('Name'))
                 eventItem.item_id = message.get('Item', {}).get('SeriesId')
                 eventItem.season_id = message.get('Item', {}).get('ParentIndexNumber')
                 eventItem.episode_id = message.get('Item', {}).get('IndexNumber')
