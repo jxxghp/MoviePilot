e2bd5cc24558816c0ad484983fbd91d6f4ce54e6
jxxghp <jxxghp@gmail.com>
2023-11-16 21:10:21 +0800

    更新 resource.py

--- app/helper/resource.py
+++ app/helper/resource.py
@@ -17,7 +17,7 @@ class ResourceHelper(metaclass=Singleton):
     # 资源包的git仓库地址
     _repo = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Resources/main/package.json"
     _files_api = f"https://api.github.com/repos/jxxghp/MoviePilot-Resources/contents/resources"
-    _base_dir: Path = settings.ROOT_PATH / "app"
+    _base_dir: Path = settings.ROOT_PATH
 
     def __init__(self):
         self.siteshelper = SitesHelper()
