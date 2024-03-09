9b9ae9401e3747cd46d709685abd41f7e9eeef3f
jxxghp <jxxghp@gmail.com>
2024-03-09 21:33:59 +0800

    fix bug

--- app/chain/__init__.py
+++ app/chain/__init__.py
@@ -325,8 +325,7 @@ class ChainBase(metaclass=ABCMeta):
 
     def transfer(self, path: Path, meta: MetaBase, mediainfo: MediaInfo,
                  transfer_type: str, target: Path = None,
-                 episodes_info: List[TmdbEpisode] = None,
-                 downloader: str = settings.DEFAULT_DOWNLOADER) -> Optional[TransferInfo]:
+                 episodes_info: List[TmdbEpisode] = None) -> Optional[TransferInfo]:
         """
         文件转移
         :param path:  文件路径
@@ -335,12 +334,10 @@ class ChainBase(metaclass=ABCMeta):
         :param transfer_type:  转移模式
         :param target:  转移目标路径
         :param episodes_info: 当前季的全部集信息
-        :param downloader:  下载器
         :return: {path, target_path, message}
         """
         return self.run_module("transfer", path=path, meta=meta, mediainfo=mediainfo,
-                               transfer_type=transfer_type, target=target,
-                               episodes_info=episodes_info, downloader=downloader)
+                               transfer_type=transfer_type, target=target, episodes_info=episodes_info)
 
     def transfer_completed(self, hashs: Union[str, list], path: Path = None,
                            downloader: str = settings.DEFAULT_DOWNLOADER) -> None:
