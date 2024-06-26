db7b2cdcacd7a0a51089890157f5ad985ee86745
jxxghp <jxxghp@gmail.com>
2024-06-26 17:42:08 +0800

    fix error

---
+++ database/versions/a40261701909_1_0_20.py
@@ -0,0 +1,21 @@
+"""1.0.20
+
+Revision ID: a40261701909
+Revises: ae9d8ed8df97
+Create Date: 2024-05-22 19:16:21.374806
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'a40261701909'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
