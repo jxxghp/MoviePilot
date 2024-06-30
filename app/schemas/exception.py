b9d91c5cd7291bb9c5fd017df195f2914ae2f710
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-06-30 11:48:29 +0800

    feat: DoubanModule触发限流时支持立即抛出限流异常

--- app/schemas/exception.py
+++ app/schemas/exception.py
@@ -4,3 +4,11 @@ class ImmediateException(Exception):
     当不希望使用重试机制时，可以抛出此异常。
     """
     pass
+
+
+class APIRateLimitException(ImmediateException):
+    """
+    用于表示API速率限制的异常类。
+    当API调用触发速率限制时，可以抛出此异常以立即终止操作并报告错误。
+    """
+    pass
