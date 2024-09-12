1ebec13afb27b88ce432125a45935328d17c1099
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-09-12 21:12:10 +0800

    feat(auth): add AUXILIARY_AUTH_ENABLE for user authentication

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -38,8 +38,8 @@ async def login_access_token(
         otp_password=otp_password
     )
     if not success:
-        # 认证不成功
-        if not user:
+        # 认证不成功且开启了辅助认证
+        if not user and settings.AUXILIARY_AUTH_ENABLE:
             # 未找到用户，请求协助认证
             logger.warn(f"登录用户 {form_data.username} 本地不存在，尝试辅助认证 ...")
             token = UserChain().user_authenticate(form_data.username, form_data.password)
