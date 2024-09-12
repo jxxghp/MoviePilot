58cd42bacac25b2d33558713d78afbedf848c1b6
Akimio521 <akimio2333@gmail.com>
2024-09-12 20:00:22 +0800

    feat:DoubanModule.recognize_media方法匹配原始中文标题名

--- app/modules/douban/__init__.py
+++ app/modules/douban/__init__.py
@@ -94,7 +94,7 @@ class DoubanModule(_ModuleBase):
             elif meta:
                 info = {}
                 # 使用中英文名分别识别，去重去空，但要保持顺序
-                names = list(dict.fromkeys([k for k in [meta.cn_name, meta.en_name] if k]))
+                names = list(dict.fromkeys([k for k in [meta.cn_name, meta.en_name, meta.origin_cn_name] if k]))
                 for name in names:
                     if meta.begin_season:
                         logger.info(f"正在识别 {name} 第{meta.begin_season}季 ...")
