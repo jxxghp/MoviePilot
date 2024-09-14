f6c84a744cb581d0d3f3f40fee310bdb01da2373
jxxghp <jxxghp@gmail.com>
2024-09-14 12:33:05 +0800

    feat：国语配音适配大陆剧

--- app/modules/filter/__init__.py
+++ app/modules/filter/__init__.py
@@ -115,7 +115,10 @@ class FilterModule(_ModuleBase):
         # 国语配音
         "CNVOI": {
             "include": [r'[国國][语語]配音|[国國]配|[国國][语語]'],
-            "exclude": []
+            "exclude": [],
+            "tmdb": {
+                "original_language": "zh"
+            }
         },
         # 粤语配音
         "HKVOI": {
