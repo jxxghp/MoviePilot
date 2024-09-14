593bcbf455e51d3c17823820204361a7c0108145
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-09-14 17:56:50 +0800

    fix(auth): set AUXILIARY_AUTH_ENABLE default to false

--- app/core/config.py
+++ app/core/config.py
@@ -63,7 +63,7 @@ class Settings(BaseSettings):
     # 超级管理员
     SUPERUSER: str = "admin"
     # 辅助认证，允许通过媒体服务器认证并创建用户
-    AUXILIARY_AUTH_ENABLE: bool = True
+    AUXILIARY_AUTH_ENABLE: bool = False
     # API密钥，需要更换
     API_TOKEN: Optional[str] = None
     # 网络代理 IP:PORT
