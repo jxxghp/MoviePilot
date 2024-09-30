06ea9e2d096aa7c01f2aab12926effe181a3ead0
jxxghp <jxxghp@gmail.com>
2024-09-30 10:26:32 +0800

    fix siteuserdata

--- app/db/site_oper.py
+++ app/db/site_oper.py
@@ -105,7 +105,7 @@ class SiteOper(DbOper):
         })
         return True, "更新站点RSS地址成功"
 
-    def update_userdata(self, domain: str, payload: dict) -> Tuple[bool, str]:
+    def update_userdata(self, domain: str, name: str, payload: dict) -> Tuple[bool, str]:
         """
         更新站点用户数据
         """
@@ -114,6 +114,7 @@ class SiteOper(DbOper):
         current_time = datetime.now().strftime('%H:%M:%S')
         payload.update({
             "domain": domain,
+            "name": name,
             "updated_day": current_day,
             "updated_time": current_time
         })
@@ -129,6 +130,12 @@ class SiteOper(DbOper):
                     payload[key] = json.dumps(value)
             SiteUserData(**payload).create(self._db)
         return True, "更新站点用户数据成功"
+
+    def get_userdata(self) -> List[SiteUserData]:
+        """
+        获取站点用户数据
+        """
+        return SiteUserData.list(self._db)
 
     def get_userdata_by_domain(self, domain: str, workdate: str = None) -> List[SiteUserData]:
         """
