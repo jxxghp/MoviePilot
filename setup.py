f03da6dacad8d3f9aed1f87d7600156d83f8685f
jxxghp <jxxghp@gmail.com>
2025-02-14 17:17:16 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-
 from distutils.core import setup
 
 from Cython.Build import cythonize
 
-
 module_list = ['app/helper/sites.py']
 
 setup(
-        name="",
-        author="",
-        zip_safe=False,
-        include_package_data=True,
-        ext_modules=cythonize(
-            module_list=module_list,
-            nthreads=0,
-            compiler_directives={"language_level": "3"},
-        ),
-        script_args=["build_ext", "-j", '2', "--inplace"],
-    )
+    name="",
+    author="",
+    zip_safe=False,
+    include_package_data=True,
+    ext_modules=cythonize(
+        module_list=module_list,
+        nthreads=0,
+        compiler_directives={
+            "language_level": "3",
+            "binding": False,
+            "nonecheck": False
+        },
+    ),
+    script_args=["build_ext", "-j", '2', "--inplace"],
+)
