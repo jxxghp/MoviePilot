8a835605fbb616118de5ca4c6590ab0a6c9f58d1
developer-wlj <developer.wlj@outlook.com>
2025-05-27 21:16:54 +0800

    fix reboot

--- app/api/endpoints/system.py
+++ app/api/endpoints/system.py
@@ -36,6 +36,7 @@ from app.schemas.types import SystemConfigKey
 from app.utils.crypto import HashUtils
 from app.utils.http import RequestUtils
 from app.utils.security import SecurityUtils
+from app.utils.system import SystemUtils
 from app.utils.url import UrlUtils
 from app.helper.system import SystemHelper
 from version import APP_VERSION
@@ -474,12 +475,10 @@ def restart_system(_: User = Depends(get_current_active_superuser)):
     """
     重启系统（仅管理员）
     """
-    if not SystemHelper.can_restart():
-        return schemas.Response(success=False, message="当前运行环境不支持重启操作！")
     # 标识停止事件
     global_vars.stop_system()
     # 执行重启
-    ret, msg = SystemHelper.restart()
+    ret, msg = SystemUtils.restart()
     return schemas.Response(success=ret, message=msg)
 
 
